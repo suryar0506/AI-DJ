bdf63c8459983dbbc9a85cd707c72f8e9d37e8b8
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-07-05 20:18:49 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("I'm not empty! - Main.js file to GitHub");
+var song = "";
+
+function preload(){
+ song = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(560, 600);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 560, 600);
+}
+
+function play(){
+    song.play();
+}
